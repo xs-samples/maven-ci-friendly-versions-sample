d1619a3c0ea9e6edc99c2d73cad2477e206f684f
whj5002120 <whj5002120@ixiancheng.com>
2020-10-31 15:47:15 +0800

    :sparkles: init

---
+++ module2/src/main/java/org/apache/maven/ci/friendly/versions/demo/App.java
@@ -0,0 +1,13 @@
+package org.apache.maven.ci.friendly.versions.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
